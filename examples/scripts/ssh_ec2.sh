7b1776122bdf92f9b35c6019f08bb20e047d0e53
ChrisSmith <chris.smith324@gmail.com>
2012-01-25 14:22:28 -0500

    updated my login

--- examples/scripts/ssh_ec2.sh
+++ examples/scripts/ssh_ec2.sh
@@ -2,4 +2,4 @@
 #Lazy script to connect to my ec2 instance
 # 
 #The instance number might change? Need to look into this
-ssh -i ~/.aws/amazon_keys.pem ec2-user@ec2-107-22-56-205.compute-1.amazonaws.com
+ssh -i ~/.aws/amazon_keys.pem ubuntu@ec2-67-202-30-112.compute-1.amazonaws.com
